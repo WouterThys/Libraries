9232b3ba36e3f8d2062daedff05a65ab30b58e62
Wouter <thys_wouter@hotmail.com>
2018-06-08 12:43:18 +0200

    UART1 lib

---
+++ Microship/DSPIC_UART/UART_Lib.X/build/default/production/UART_Driver.o.d
@@ -0,0 +1,3 @@
+build/default/production/UART_Driver.o:  \
+UART_Driver.c  \
+UART_Driver.h 
---
+++ Microship/DSPIC_UART/UART_Lib.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=UART_Lib.X.a
+CND_ARTIFACT_PATH_default=dist/default/production/UART_Lib.X.a
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=UARTLib.X.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/UARTLib.X.tar
